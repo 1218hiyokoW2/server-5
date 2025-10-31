59a6cc45915de4c076e0a2eef27467f39df1aae5
1218hiyokoW2 <192060988+1218hiyokoW2@users.noreply.github.com>
2025-10-31 12:17:54 +0900

    POSTリクエスト（フォームデータ）追加

--- server.js
+++ server.js
@@ -4,27 +4,32 @@ const app = new Hono();
 
 app.use('/*', serveStatic({ root: './public' }));
 
-// GETリクエストに対する処理（クエリーパラメーター）
-app.get('/api', async (c) => {
-  // パラメーターの受け取り
-  const name = c.req.query('name');
-  const rank = c.req.query('rank');
-
-  return c.json({ message: 'GET', query: { name, rank } });
-});
-
-// // GETリクエストに対する処理（パスパラメーター）
-// app.get('/api/:name/:rank', (c) => {
+// // GETリクエストに対する処理（クエリーパラメーター）
+// app.get('/api', async (c) => {
 //   // パラメーターの受け取り
-//   const name = c.req.param('name');
-//   const rank = c.req.param('rank');
+//   const name = c.req.query('name');
+//   const rank = c.req.query('rank');
 
-//   return c.json({ message: 'GET', param: { name, rank } });
+//   return c.json({ message: 'GET', query: { name, rank } });
 // });
+
+// GETリクエストに対する処理（パスパラメーター）
+app.get('/api/:name/:rank', (c) => {
+  // パラメーターの受け取り
+  const name = c.req.param('name');
+  const rank = c.req.param('rank');
+
+  return c.json({ message: 'GET', param: { name, rank } });
+});
 
 // POSTリクエストに対する処理
 app.post('/api', async (c) => {
-  return c.json({ message: 'POST' });
+  // メッセージボディの受け取り
+  const body = await c.req.parseBody();
+  const name = body['name'];
+  const rank = body['rank'];
+
+  return c.json({ message: 'POST', form: { name, rank } });
 });
 
 // PUTリクエストに対する処理
